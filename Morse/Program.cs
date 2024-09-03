64e504bc8d7c4216302719c37f3ba23e72241570
Jonas01101111 <jonas1kiubi@gmail.com>
2024-09-03 12:24:06 -0300

    Adicionar arquivos de projeto.

---
+++ Morse/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morse
+{
+    internal class Program
+    {
+        static string ToMorse(string letra) 
+        {
+            string codificado;
+
+            switch (letra) 
+            {
+                case "a":
+                    codificado = "";
+                    break;
+                case "b":
+                    codificado = "";
+                    break;
+                case "c":
+                    codificado = "";
+                    break;
+                case "d":
+                    codificado = "";
+                    break;
+                case "e":
+                    codificado = "";
+                    break;
+                case "f":
+                    codificado = "";
+                    break;
+                case "g":
+                    codificado = "";
+                    break;
+                case "h":
+                    codificado = "";
+                    break;
+                case "i":
+                    codificado = "";
+                    break;
+                case "j":
+                    codificado = "";
+                    break;
+                case "k":
+                    codificado = "";
+                    break;
+                case "l":
+                    codificado = "";
+                    break;
+                case "m":
+                    codificado = "";
+                    break;
+                case "n":
+                    codificado = "";
+                    break;
+                case "o":
+                    codificado = "";
+                    break;
+                case "p":
+                    codificado = "";
+                    break;
+                case "q":
+                    codificado = "";
+                    break;
+                case "r":
+                    codificado = "";
+                    break;
+                case "s":
+                    codificado = "";
+                    break;
+                case "t":
+                    codificado = "";
+                    break;
+                case "u":
+                    codificado = "";
+                    break;
+                case "v":
+                    codificado = "";
+                    break;
+                case "w":
+                    codificado = "";
+                    break;
+                case "x":
+                    codificado = "";
+                    break;
+                case "y":
+                    codificado = "";
+                    break;
+                case "z ":
+                    codificado = "";
+                    break;
+
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {   
+            string texto = "aaaa";
+            string codigo =""
+
+            foreach (char c in texto[]) 
+            {
+            
+            }
+
+
+        }
+    }
+}
